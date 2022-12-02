35ed17cfdb99410fc74a211424f2840ada18c2fd
abreudesarah <sarah.abreufernandes@gmail.com>
2022-12-02 12:00:19 -0300

    formulario

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Usuario } from '../models/Usuario';
+import { StorageService } from '../services/storage.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  listaUsuarios: Usuario[] = [];
+
+  constructor(private storageService: StorageService) { }
+
+  async buscarUsuarios(){
+    this.listaUsuarios = await this.storageService.getAll();
+  }
+
+ionViewDidEnter() {
+  this.buscarUsuarios();
+}
+
+ async excluirCadastro(email: string) {
+  await this.storageService.remove(email);
+  this.buscarUsuarios();
+}
+
+}
